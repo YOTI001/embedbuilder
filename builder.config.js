49ef4c34011d8cde523a9ad530be20112ce60562
drgato <mathisebas18@gmail.com>
2022-04-02 17:29:38 -0500

    first commit

---
+++ builder.config.js
@@ -0,0 +1,31 @@
+// URL options can override the options below.
+// Options set through the menu can override both.
+options = {
+	username: 'LunarBot',
+	avatar: 'https://cdn.discordapp.com/avatars/944332616558075954/36c90de840283d2d0a544303bcae8d7c.png?size=1024',
+	verified: true,
+	noUser: false,
+	dataSpecified: false,
+	guiTabs: ['author', 'description'],
+	useJsonEditor: false,
+	reverseColumns: false,
+	allowPlaceholders: true,
+	autoUpdateURL: false,
+	autoParams: false,
+	hideEditor: false,
+	hidePreview: false,
+	hideMenu: false
+};
+
+// // Default JSON object
+// json = {
+// 	content: null,
+// 	embed: {
+// 		title: 'A title',
+// 		description: 'A description'
+// 	}
+// };
+
+onload = () => {
+	// console.log('Salut 👋');
+};
