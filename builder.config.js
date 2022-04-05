07776f3210b9ab0216d68609791e124df1340afe
YOTI001 <88567132+YOTI001@users.noreply.github.com>
2022-04-05 02:12:27 -0500

    Update builder.config.js

--- builder.config.js
+++ builder.config.js
@@ -1,8 +1,8 @@
 // URL options can override the options below.
 // Options set through the menu can override both.
 options = {
-	username: 'LunarBot',
-	avatar: 'https://cdn.discordapp.com/avatars/944332616558075954/36c90de840283d2d0a544303bcae8d7c.png?size=1024',
+	username: 'Lumity',
+	avatar: 'https://images-ext-2.discordapp.net/external/7lEvaYeavf_60KKVnUKUmsDE3CF57hgpj7BKLZr9vHw/%3Fsize%3D512/https/cdn.discordapp.com/avatars/948629004225744926/5ba6c82bcf1ae50cb390ba5563bec1f0.webp?width=341&height=341',
 	verified: true,
 	noUser: false,
 	dataSpecified: false,
